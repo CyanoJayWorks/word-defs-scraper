45aa03de4139f02ea4a8f5f8ca092d8110431f97
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-06-24 14:18:39 -0700

    adding initial files

---
+++ src/Scraper.java
@@ -0,0 +1,26 @@
+
+public class Scraper {
+	private String[] wordList;
+	
+	public static void main(String args[]) {
+		new Scraper().start();
+	}
+	
+	private void start() {
+		populateWordList();
+		mapWordsToDefinitions();
+		outputWordDefinitionPairs();
+	}
+
+	private void populateWordList() {
+		
+	}
+
+	private void mapWordsToDefinitions() {
+		
+	}
+
+	private void outputWordDefinitionPairs() {
+		
+	}
+}
