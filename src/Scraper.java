f3572f0c76e22f67c0287c4450d55d90b8a0a12b
Ajay Gopinath <ajay.gopinath@outlook.com>
2013-06-24 16:21:58 -0700

    finished all networking / json parsing / file outputting

--- src/Scraper.java
+++ src/Scraper.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +19,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.agopinath.lthelogutil.Fl;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 
 public class Scraper {
@@ -26,23 +31,24 @@ public class Scraper {
 	private HttpClient client;
 	private List<String> wordList;
 	private Map<String, String> worddefpairs;
-	
+	private String inputFile = "wordlist/sat-barrons-main.txt";
+	private String outputFile = "output/" + inputFile.substring(inputFile.indexOf('/'), inputFile.indexOf('.')) + "-defs.txt";
 	
 	public static void main(String args[]) {
 		new Scraper().start();
 	}
 	
 	private void start() {
-		populateWordList("wordlist/sat-barrons-main.txt");
+		populateWordList();
 		mapWordsToDefinitions();
 		outputWordDefinitionPairs();
 	}
 
-	private void populateWordList(String wordFile) {
+	private void populateWordList() {
 		wordList = new ArrayList<String>();
 		Scanner s = null;
 		try {
-			s = new Scanner(new File(wordFile));
+			s = new Scanner(new File(inputFile));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -57,19 +63,61 @@ public class Scraper {
 		worddefpairs = new HashMap<String, String>();
 		client = new DefaultHttpClient();
 		
+		JsonParser parser = new JsonParser();
+		JsonArray defs = null;
+		int numDefsWanted = 2;
+		
+		//int j = 0;
 		for(String word : wordList) {
-			String gson = getWordDataGson(word);
+			String json = getWordDataJson(word);
+			int idx = 0;
+			boolean success = true;
+			do {
+				
+				try {
+					defs = parser.parse(json)
+							.getAsJsonObject().get("tuc")
+							.getAsJsonArray().get(idx++)
+							.getAsJsonObject().get("meanings")
+							.getAsJsonArray();
+					success = true;
+				} catch(NullPointerException e) {
+					success = false;
+				}
+			} while(!success);
 			
+			Fl.og(defs.toString());
+			
+			int count = 0;
+			for(JsonElement result : defs) {
+				String defText = result.getAsJsonObject().get("text").getAsString();
+				String currDefs = worddefpairs.get(word);
+				if(currDefs != null) {
+					worddefpairs.put(word, currDefs + "\n\t " + (count+1) + ". " + defText);
+				} else {
+					worddefpairs.put(word, (count+1) + ". " + defText);
+				}
+				
+				count++;
+				if(count == numDefsWanted) break;
+			}
+			
+			//j++;
+			//if(j == 30) break;
 		}
+		
+		/*for(String word : worddefpairs.keySet()) {
+			String def = worddefpairs.get(word);
+			Fl.og(String.format("%2s : %2s", word, def));
+		}*/
 	}
 	
-	private String getWordDataGson(String word) {
+	private String getWordDataJson(String word) {
 		HttpGet get = new HttpGet(API_CALL_PREFIX + word + API_CALL_SUFFIX);
 		String json = null;
 		
 		try {
 			HttpResponse response = client.execute(get);
-			Fl.og("Received response from GET for word: " + word);
 
 			HttpEntity entity = response.getEntity();
 
@@ -114,6 +162,19 @@ public class Scraper {
 	}
 	
 	private void outputWordDefinitionPairs() {
+		File output = new File(outputFile);
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(new FileWriter(output));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		for(String word : worddefpairs.keySet()) {
+			String def = worddefpairs.get(word);
+			String line = word + ", " + def + "\n";
+			Fl.og(line);
+			writer.write(line);
+		}
 	}
 }
